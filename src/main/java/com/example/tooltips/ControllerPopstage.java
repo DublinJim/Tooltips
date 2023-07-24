a2d070ba5501cb8c3f1884c32b177a6ddf0926f6
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-07-24 12:24:42 -0400

    adds a new stage . closing correctly

--- src/main/java/com/example/tooltips/ControllerPopstage.java
+++ src/main/java/com/example/tooltips/ControllerPopstage.java
@@ -1,6 +1,5 @@
 package com.example.tooltips;
 
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
@@ -18,32 +17,33 @@ public class ControllerPopstage implements Initializable {
     public Text txtInfo;
     public Button popBtn;
     public AnchorPane rootPanePOP;
-    private String infoToDisplay;
-    private Stage popStage = new Stage();
 
+    private String infoToDisplay;
+private  Stage cancelStage = new Stage();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         infoToDisplay = "In this example, we create a custom alert by creating" + " a new Stage and populating it with a VBox containing a label and" + " an OK button. We set the background color of the VBox using CSS" + " with the -fx-background-color property. T" + "he custom alert is displayed with a light blue (" + "#e0f0ff) background color.Keep in mind that customizing the appearance" + " of an alert this way might not provide the same consistent look and feel as the standard " + "system alert. Customizing alerts may be useful when you want a specific design or have " + "unique requirements, but it may not fully match the native look and behavior of standard alerts.";
         txtInfo.setText(infoToDisplay);
-
-        popBtn.setOnAction(actionEvent -> closePopup());
+        popBtn.setOnAction(e -> closePopup());
     }
 
 
-
     public void showStage() throws IOException {
+        Stage popStage = new Stage(StageStyle.UNDECORATED);
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("popstage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         popStage.setScene(scene);
-        popStage.initStyle(StageStyle.UNDECORATED);
+
         popStage.show();
-      //  popStage.close();
+cancelStage=popStage;
 
     }
+
     private void closePopup() {
-        popStage.close();
-        System.out.println("out");
+        Scene currentScene = popBtn.getScene();
+        cancelStage = (Stage) currentScene.getWindow();
+        cancelStage.close();
     }
 
 }
