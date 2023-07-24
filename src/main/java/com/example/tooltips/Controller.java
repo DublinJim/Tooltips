2069540dde1fd14efbee91b4c32d054a68f39ce1
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-07-24 11:25:27 -0400

    adds a new stage ..not closing correctly

--- src/main/java/com/example/tooltips/Controller.java
+++ src/main/java/com/example/tooltips/Controller.java
@@ -1,5 +1,6 @@
 package com.example.tooltips;
 
+import javafx.event.Event;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
@@ -7,6 +8,7 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Popup;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,37 +20,55 @@ public class Controller implements Initializable {
     public CheckBox chkBox1;
     public Button btn1;
     public Label lbl1;
+    public Label lblChoice;
+
+public ControllerPopstage controllerPopstage = new ControllerPopstage();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //the pop up
+        //the pop-up
         Popup popup = new Popup();
         Label popLbl = new Label("Information on stuff right here");
         popup.getContent().add(popLbl);
         popup.setAutoHide(true);
 
 
+
+
 //........................
-        Tooltip chkTip = new Tooltip("This is the explanation of what this does.\n More stuff on a new line");
-
-        chkTip.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
-
+        Tooltip chkTip = getTooltip();
 
         lbl1.setTooltip(chkTip);
-        lbl1.setOnMouseClicked(mouseEvent -> {
+        lbl1.setOnMouseClicked(mouseEvent -> alertDisplay());
 
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Warning Dialog");
-            alert.setHeaderText(null);
-            alert.setContentText("This is a warning message.");
-            alert.initStyle(StageStyle.UNDECORATED);
-           alert.initStyle(StageStyle.UTILITY);
-            alert.showAndWait();
-
+        lblChoice.setOnMouseClicked(mouseEvent -> {
+            try {
+                controllerPopstage.showStage();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
-        chkTip.setShowDelay(javafx.util.Duration.millis(100));
+
+
+        lblChoice.setTooltip(chkTip);
 
     }//init end
 
+    private static Tooltip getTooltip() {
+        Tooltip chkTip = new Tooltip("Whats this ?\nClick for more info");
+        chkTip.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
+        chkTip.setShowDelay(javafx.util.Duration.millis(100));
 
+        return chkTip;
+    }
+
+    private static void alertDisplay() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Warning Dialog");
+        alert.setHeaderText(null);
+        alert.setContentText("This is a warning message.");
+        alert.initStyle(StageStyle.UNDECORATED);
+        alert.initStyle(StageStyle.UTILITY);
+        alert.showAndWait();
+    }
 }//end
--- src/main/java/com/example/tooltips/ControllerPopstage.java
+++ src/main/java/com/example/tooltips/ControllerPopstage.java
@@ -1,28 +1,49 @@
 package com.example.tooltips;
 
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerPopstage implements Initializable {
-    public AnchorPane rootPane;
     public Text txtInfo;
     public Button popBtn;
+    public AnchorPane rootPanePOP;
     private String infoToDisplay;
+    private Stage popStage = new Stage();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         infoToDisplay = "In this example, we create a custom alert by creating" + " a new Stage and populating it with a VBox containing a label and" + " an OK button. We set the background color of the VBox using CSS" + " with the -fx-background-color property. T" + "he custom alert is displayed with a light blue (" + "#e0f0ff) background color.Keep in mind that customizing the appearance" + " of an alert this way might not provide the same consistent look and feel as the standard " + "system alert. Customizing alerts may be useful when you want a specific design or have " + "unique requirements, but it may not fully match the native look and behavior of standard alerts.";
         txtInfo.setText(infoToDisplay);
 
-        popBtn.setOnAction(actionEvent -> Platform.exit());
+        popBtn.setOnAction(actionEvent -> closePopup());
     }
 
 
+
+    public void showStage() throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("popstage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        popStage.setScene(scene);
+        popStage.initStyle(StageStyle.UNDECORATED);
+        popStage.show();
+      //  popStage.close();
+
+    }
+    private void closePopup() {
+        popStage.close();
+        System.out.println("out");
+    }
 
 }
