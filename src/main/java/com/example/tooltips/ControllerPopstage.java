aabf57012fb44bfc6df8392ff7eef5b9956c87f0
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-07-24 14:04:09 -0400

    removes dead code

--- src/main/java/com/example/tooltips/ControllerPopstage.java
+++ src/main/java/com/example/tooltips/ControllerPopstage.java
@@ -22,8 +22,7 @@ public class ControllerPopstage implements Initializable {
 private  Stage cancelStage = new Stage();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        infoToDisplay = "In this example, we create a custom alert by creating" + " a new Stage and populating it with a VBox containing a label and" + " an OK button. We set the background color of the VBox using CSS" + " with the -fx-background-color property. T" + "he custom alert is displayed with a light blue (" + "#e0f0ff) background color.Keep in mind that customizing the appearance" + " of an alert this way might not provide the same consistent look and feel as the standard " + "system alert. Customizing alerts may be useful when you want a specific design or have " + "unique requirements, but it may not fully match the native look and behavior of standard alerts.";
-        txtInfo.setText(infoToDisplay);
+
         popBtn.setOnAction(e -> closePopup());
     }
 
